91e5fc77ea5a125ccd9a45967b973d62edfd5c19
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-06-17 19:43:19 +0330

    feat: adding GetDB method for postgres
    GetDB return instance of gorm DB for postgres

---
+++ internal/database/postgres/service.go
@@ -0,0 +1,31 @@
+package postgres
+
+import (
+	"sync"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+	once sync.Once
+)
+
+func GetDB() *gorm.DB {
+	once.Do(func() {
+		dsn, err := GetDSN()
+		if err != nil {
+			panic("Failed to get DSN Postgres: " + err.Error())
+		}
+
+		conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic("Failed to open connection to Postgres: " + err.Error())
+		}
+
+		db = conn
+	})
+
+	return db
+}
