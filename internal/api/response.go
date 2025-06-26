02e138ddb68e2e9fbdcfbe0c3f0828d844e7ea9f
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-06-27 02:49:02 +0330

    feat: adding  utilit method for api response

--- internal/api/response.go
+++ internal/api/response.go
@@ -1,5 +1,11 @@
 package api
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type ErrorResponse struct {
 	Error   string `json:"error"`
 	Message string `json:"message,omitempty"`
@@ -8,4 +14,25 @@ type ErrorResponse struct {
 type SuccessResponse struct {
 	Data    any    `json:"data,omitempty"`
 	Message string `json:"message,omitempty"`
+}
+
+func RespondAPIError(c *gin.Context, apiErr *APIError) {
+	c.JSON(apiErr.StatusCode, ErrorResponse{
+		Error:   apiErr.Error,
+		Message: apiErr.Message,
+	})
+}
+
+func RespondInternalError(c *gin.Context, err error) {
+	c.JSON(http.StatusInternalServerError, ErrorResponse{
+		Error: "Internal Server Error",
+		Message: err.Error(),
+	})
+}
+
+func RespondSuccess(c *gin.Context, statusCode int, msg string, data any) {
+	c.JSON(statusCode, SuccessResponse{
+		Message: msg,
+		Data: data,
+	})
 }
